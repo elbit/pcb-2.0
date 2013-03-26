70091726a58b8e3aca1a9035409fa8bc7ebf9e34
elbit.cat <elbitcat@gmail.com>
2013-03-26 18:34:10 +0100

    v.0.2.1 / to php / logo petit / cta contacte

---
+++ index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Produccions Contrabaix</title>
+        <meta name="description" content="Produccions Contrabaix">
+        <meta name="viewport" content="width=device-width">
+
+       
+
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+        <![endif]-->
+
+        
+    <header >
+        <div class="width-1-3 logo">
+            <img  src="img/logo-pcb.jpg" alt="home"> 
+        </div> 
+        <a class="contacte float-right" href="mailto:info@produccionscontrabaix.com">info@produccionscontrabaix.com</a> 
+    
+    </header> 
+    
+    <section>
+        <h1> Difusi&oacute; cultural</h1>
+        <div class="module width-1-2">
+            <a href="http://contrabaix.com" target="_blank"><img src="img/logo-contrabaix.jpg"></a>
+        </div>
+        <div class="module width-1-2">
+            <a href="http://popaprop.cat" target="_blank"><img src="img/logo-popaprop.png"></a>
+        </div>
+       
+    </section>
+    
+     <section>
+        <h1>Managment i producci&oacute; jazz</h1>
+        <div class="module width-1-3 musics">
+            <a href="http://javiercolina.com" target="_blank"><img src="img/javiercolina.jpg"></a>
+            <h2>Javier Colina</h2>
+        </div>
+        <div class="module width-1-3 musics">
+            <a href="http://www.marcmiralta.com/biografia.htm" target="_blank"><img src="img/miralta.jpg"></a>
+            <h2>Marc Miralta</h2>
+        </div>
+        <div class="module width-1-3 musics">
+            <a href="http://pericosambeat.com" target="_blank"><img src="img/perico.jpg"></a>
+            <h2>Perico Sambeat</h2>
+        </div>
+        <div class="module width-1-3 musics">
+             <a href="http://pericosambeat.com/cms/" target="_blank"><img src="img/cms.jpg"></a>
+             <h2>Colina Miralta Sambeat CMS Trio</h2>
+        </div>
+        <div class="module width-1-3 musics">
+            <a href="http://www.michael-wollny.de/" target="_blank"><img src="img/wollny.jpg"></a>
+            <h2>Michael Wollny</h2>
+        </div>
+        <div class="module width-1-3 musics">
+            <a href="http://enlaimaginacion.com" target="_blank"><img src="img/enlaimaginacion.jpg"></a>
+            <h2>Sílvia Pérez Cruz y Javier Colina</h2>
+        </div>
+        
+    </section>
+    
+    
+    <footer>
+        
+      Produccions ContraBaix <?php echo date("Y");?> <span class="separador">|</span> Vidal i Ribas, 25. 08980 Sant Feliu de Llobregat. Barcelona. <span class="separador">|</span> +34 936 853 242   <span class="separador">|</span> 
+      <a class="contacte" href="mailto:info@produccionscontrabaix.com">info@produccionscontrabaix.com</a>  
+        
+    </footer>
+        
+        
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.9.0.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+        
+        <script>
+            var _gaq=[['_setAccount','UA-7047554-1'],['_trackPageview']];
+            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+            g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+            s.parentNode.insertBefore(g,s)}(document,'script'));
+        </script>
+    </body>
+</html>
